e775f1e6e0ecbceb62d507ded9bc00a4d0099042
Alex Siryi <desertkun@gmail.com>
2017-01-21 17:16:54 +0200

    Fixed store setup

--- sql/tier_components.sql
+++ sql/tier_components.sql
@@ -1,8 +1,10 @@
-CREATE TABLE `pack_components` (
+CREATE TABLE `tier_components` (
   `component_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `pack_id` int(11) NOT NULL,
+  `tier_id` int(11) unsigned NOT NULL,
   `gamespace_id` int(11) NOT NULL,
   `component` varchar(32) NOT NULL DEFAULT '',
   `component_data` json NOT NULL,
-  PRIMARY KEY (`component_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`component_id`),
+  KEY `tier_id` (`tier_id`),
+  CONSTRAINT `tier_components_ibfk_1` FOREIGN KEY (`tier_id`) REFERENCES `tiers` (`tier_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/tiers.sql
+++ sql/tiers.sql
@@ -1,10 +1,12 @@
-CREATE TABLE `packs` (
-  `pack_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tiers` (
+  `tier_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `gamespace_id` int(11) NOT NULL,
-  `store_id` int(11) NOT NULL,
-  `pack_name` varchar(255) NOT NULL DEFAULT '',
-  `pack_product` varchar(255) NOT NULL DEFAULT '',
-  `pack_prices` json NOT NULL,
-  PRIMARY KEY (`pack_id`),
-  UNIQUE KEY `pack_name` (`pack_name`)
+  `store_id` int(11) unsigned NOT NULL,
+  `tier_name` varchar(255) NOT NULL DEFAULT '',
+  `tier_product` varchar(255) NOT NULL DEFAULT '',
+  `tier_prices` json NOT NULL,
+  PRIMARY KEY (`tier_id`),
+  UNIQUE KEY `tier_name` (`tier_name`),
+  KEY `store_id` (`store_id`),
+  CONSTRAINT `tiers_ibfk_1` FOREIGN KEY (`store_id`) REFERENCES `stores` (`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
